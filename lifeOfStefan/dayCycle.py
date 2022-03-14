3bcb70fb33e658eb2f95bd1ac013478da0305c85
Jeppe Skovby Bjørn <34233264+jbjarn17@users.noreply.github.com>
2022-03-14 13:34:08 +0100

    initial commit

---
+++ lifeOfStefan/dayCycle.py
@@ -0,0 +1,7 @@
+class DayCycle:
+    my_int = 5
+
+    def get_int(self):
+        print("Test")
+        return self.my_int
+
---
+++ lifeOfStefan/main.py
@@ -0,0 +1,9 @@
+from dayCycle import DayCycle
+
+def func_name():
+    i = DayCycle()
+    print(i.get_int())
+
+if __name__ == "__main__":
+    print("Hello World")
+    func_name()
