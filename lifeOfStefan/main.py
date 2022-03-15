ac5561263f3029ec38238f38339eb8997ec3e30c
Jeppe Bjørn <jbjarn17@student.aau.dk>
2022-03-15 20:16:04 +0100

    Update main.py

--- lifeOfStefan/main.py
+++ lifeOfStefan/main.py
@@ -2,12 +2,16 @@ import time
 
 from dayCycle import DayCycle
 
+def main():
+    i = DayCycle(5)
+
+    i.increase_time()
+    i.increase_time()
+
+    print(i.time_of_day())
+
+
 if __name__ == "__main__":
     print("Hello World")
+    main()
 
-i = DayCycle(5)
-
-i.increase_time()
-i.increase_time()
-
-print(i.time_of_day())
